914da226f1faf27beb95bdeb26c8a95147e7abaa
elijah jerry <benlexlee@gmail.com>
2026-05-07 08:16:51 -0500

    add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from '../lib/ThemeContext';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics /> {/* Add this line */}
     </ThemeProvider>
   );
 }
